2fbc678d3bff899a1bbd6e20b9238d62118bb169
Igor Sthanny <igorsthaynny@gmail.com>
2025-02-21 23:05:49 -0300

    feat: criando documentação para facilitar a visualizaçao dos tamanhos da fonts

--- lib/src/utils/ds_spacing.dart
+++ lib/src/utils/ds_spacing.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
 enum DSSpacing {
+  /// Extra small spacing: 2
   xxxs(2),
+
+  /// Extra small spacing: 4
   xxs(4),
+
+  /// Extra small spacing: 8
   xs(8),
+
+  /// Small spacing: 12
   sm(12),
+
+  /// Medium spacing: 16
   md(16),
+
+  /// Large spacing: 24
   lg(24),
+
+  /// Extra large spacing: 32
   xl(32),
+
+  /// Extra extra large spacing: 40
   xxl(40),
-  xxxl(64);
+
+  /// Extra extra extra large spacing: 48
+  xxxl(48),
+
+  /// Extra extra extra large spacing: 56
+  xxxxl(56),
+
+  /// Giga large spacing: 64
+  gl(64);
 
   const DSSpacing(this.value);
   final double value;
