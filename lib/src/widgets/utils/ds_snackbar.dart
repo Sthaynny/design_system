0e18df48214e885014c0e078889c162576f313da
Igor Sthanny <igorsthaynny@gmail.com>
2025-03-10 22:26:36 -0300

    feat: criando snackbar

--- lib/src/widgets/utils/ds_snackbar.dart
+++ lib/src/widgets/utils/ds_snackbar.dart
@@ -1,6 +1,7 @@
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
+
 class DSSnackBar extends SnackBar {
   DSSnackBar._({
     required String text,
@@ -8,14 +9,19 @@ class DSSnackBar extends SnackBar {
     Color? backgroundColor,
     super.action,
   }) : super(
-         content: DSCaptionSmallText(
-           text,
-           color: textColor ?? DSColors.neutralDarkCity,
-         ),
-         backgroundColor: backgroundColor ?? DSColors.primary,
-       );
+          content: DSCaptionSmallText(
+            text,
+            color: textColor ?? DSColors.neutralDarkCity,
+            maxLines: 3,
+          ),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: backgroundColor ?? DSColors.primary,
+        );
 
-  factory DSSnackBar.error(String text, {SnackBarAction? action}) =>
+  factory DSSnackBar.error(
+    String text, {
+    SnackBarAction? action,
+  }) =>
       DSSnackBar._(
         text: text,
         backgroundColor: DSColors.error,
@@ -23,20 +29,29 @@ class DSSnackBar extends SnackBar {
         action: action,
       );
 
-  factory DSSnackBar.success(String text, {SnackBarAction? action}) =>
+  factory DSSnackBar.success(
+    String text, {
+    SnackBarAction? action,
+  }) =>
       DSSnackBar._(
         text: text,
         backgroundColor: DSColors.success,
         textColor: DSColors.black,
         action: action,
       );
-  factory DSSnackBar.warning(String text, {SnackBarAction? action}) =>
+  factory DSSnackBar.warning(
+    String text, {
+    SnackBarAction? action,
+  }) =>
       DSSnackBar._(
         text: text,
         backgroundColor: DSColors.warning,
         textColor: DSColors.primary,
         action: action,
       );
-  factory DSSnackBar.info(String text, {SnackBarAction? action}) =>
+  factory DSSnackBar.info(
+    String text, {
+    SnackBarAction? action,
+  }) =>
       DSSnackBar._(text: text, textColor: DSColors.white, action: action);
 }
